f32c03abcfd9f6a3bddb812eb22ba622f311d736
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-07 18:11:34 +0300

    Adding function getAllUsers

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    
+
+    public function getAllUsers($id = null){
+        if($id != null){
+            $restos = User::find($id);
+            //$restos = $restos? $restos->name : '';
+        }else{
+            $restos = User::all();
+        }
+        
+        return response()->json([
+            "status" => "Success",
+            "restos" => $restos
+        ], 200);
+    }
+
+    /*public function signUp(Request $request, $user_type_id){
+        die($request);
+        $user = [];
+        $user["first_name"] = $request->fnamez;
+        $user["last_name"] = $request->lnamez;
+        $user["user_type_id"] = $user_type_id;
+
+        return response()->json([
+            "status" => "Success",
+            "users" => $user
+        ], 200);
+    }*/
 }
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\TestController;
+use App\Http\Controllers\UserController;
 
 
 Route::get('/hi', [TestController::class, 'sayHi'])->name('say-hi');
+Route::get('/all_users/{id?}', [UserController::class, 'getAllUsers']);
+//Route::get('/register/{user_type_id}', [TestController::class, 'signUp'])->name('sign-up');
 
 
