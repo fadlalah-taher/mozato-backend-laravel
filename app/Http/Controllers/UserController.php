f5390ded827895c2f2e488890eb785300c77d5a3
fadlalah-taher <fadeltaher111@gmail.com>
2022-06-07 21:19:36 +0300

    Cleaning code UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -55,26 +55,4 @@ class UserController extends Controller
         }
     }
 
-   /* public function getUserByName($name){
-        $user = User::where("name", "LIKE", "%$name%")->get();
-        
-        return response()->json([
-            "status" => "Success",
-            "results" => $user
-        ], 200);
-    }*/
-
-
-    /*public function signUp(Request $request, $user_type_id){
-        die($request);
-        $user = [];
-        $user["first_name"] = $request->fnamez;
-        $user["last_name"] = $request->lnamez;
-        $user["user_type_id"] = $user_type_id;
-
-        return response()->json([
-            "status" => "Success",
-            "users" => $user
-        ], 200);
-    }*/
 }
